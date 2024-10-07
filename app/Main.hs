0ebcaacac5b8270e896995468a9d23b6d9ede0fb
Rebo100 <bosgraafreinout@outlook.com>
2024-10-07 22:37:16 +0200

    gv

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,14 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use newtype instead of data" #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# LANGUAGE GADTs #-}
 module Main(main) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game (SpecialKey(KeyPad2))
 
 window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
+window = InWindow (printEntity(ShipEntity (Ship (EntityAttributes (1, 0) 0 0 0)))) (200, 200) (10, 10)
 
 background :: Color
 background = white
@@ -19,24 +20,25 @@ main :: IO ()
 main = display window background drawing
 
 
---Data types
+--      Data types
 --Entities
 -- data Entity location = Ship | Asteroid | PowerUp | Bullet
-data Entity = Ship | Asteroid | PowerUp | Bullet {
-    entityAttributes :: EAttributes
+data Entity = ShipEntity Ship
+            | AsteroidEntity Asteroid
+            | PowerUpEntity PowerUp
+            | BulletEntity Bullet
+
+data EntityAttributes = EntityAttributes {
+ position :: Position,
+ direction :: Direction,
+ speed :: Speed,
+ size :: Size
 }
 
 type Position = (Int, Int)
 type Direction = Int
 type Speed = Float
 type Size = Float
-
-data EAttributes = EAttributes {
- pst :: Position,
- dir :: Direction,
- sp :: Speed,
- size :: Size
-}
 
 --Stats
 type Lives = Int
@@ -47,23 +49,30 @@ data Stats = Stats {
 }
 
 --PowerUps
-data PowerUp a = TrippleShot | Invincibility | ExtraLife | SpeedBoost {
-    value :: a,
-    effect :: a -> a
-}
+data PowerUp = PowerUp {
+    powerUpAttributes :: EntityAttributes
+} --TrippleShot | Invincibility | ExtraLife | SpeedBoost
 
 emptyPowerUp :: PowerUp
-emptyPowerUp = ExtraLife 0
+emptyPowerUp = PowerUp {}
 
 --Bullets
-data Bullet = MkBullet {
+data Bullet = Bullet {
+    bulletAttributes :: EntityAttributes,
     count :: Int
 }
 --Ship
-data Player = P1 | P2
-type Score = Int
-type FiringRate = Float
+data Ship = Ship {
+    shipAttributes :: EntityAttributes
+    -- test :: String
+    -- playerInfo :: PlayerInfo
+}
+ 
 
+-- data ShipAttributes = ShipAttributes {
+--     attributes :: EntityAttributes,
+--     playerInfo :: PlayerInfo
+-- }
 
 data PlayerInfo = PlayerInfo {
     player :: Player,
@@ -74,12 +83,13 @@ data PlayerInfo = PlayerInfo {
     playerFiringRate :: FiringRate
 }
 
-data Ship = MkShip {
-    playerInfo :: PlayerInfo
-}
+data Player = P1 | P2
+type Score = Int
+type FiringRate = Float
 
 --Asteroid
-data Asteroid = MkAsteroid {
+data Asteroid = Asteroid {
+    asteroidAttributes :: EntityAttributes,
     asteroidStats :: Stats
 }
 
@@ -88,6 +98,18 @@ data Level = Lvl1 | Lvl2 | Lvl3 | CustomLvl {
     entities :: [Entity]
 }
 
+--Methods
+getAttributes :: Entity -> EntityAttributes
+getAttributes (ShipEntity ship) = shipAttributes ship
+getAttributes (AsteroidEntity asteroid) = asteroidAttributes asteroid
+getAttributes (PowerUpEntity powerUp)  = powerUpAttributes powerUp
+getAttributes (BulletEntity bullet)   = bulletAttributes bullet
 
--- print :: Entity -> String
--- print eAttributes = eAttributes
+printEntity :: Entity -> String
+printEntity e = sPosition ++ sDirection ++ sSpeed ++ sSize
+  where
+    sPosition = show $ position a
+    sDirection = show $ direction a
+    sSpeed = show $ speed a
+    sSize = show $ size a
+    a = getAttributes e
