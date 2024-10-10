9a2f52babf56af8142928625cdc8b55f13b684d7
Rebo100 <78788432+Rebo100@users.noreply.github.com>
2024-10-10 14:47:48 +0200

    gv

--- src/Menu's.hs
+++ src/Menu's.hs
@@ -2,10 +2,13 @@ module Menu's where
 
     import Graphics.Gloss
     import Toolbox
-    
+
     type StartMenu = [Button]
     startMenu :: StartMenu
-    startMenu = [Button "startButton" "" [(-180, -100), (100, 100)] (makeColorI 141 141 141 255)]
+    startMenu = [
+        Button "startButton" "Start" [(-120, -80), (120, -30)] (makeColorI 141 141 141 255),
+        Button "exitButton" "Exit" [(-100, -150), (100, -100)] (makeColorI 141 141 141 255)
+        ]
 
     data Button = Button {
     buttonName :: String,
@@ -21,11 +24,30 @@ module Menu's where
 
 
     drawSelectedButton :: Button -> Maybe Color -> Picture
-    drawSelectedButton button c = 
+    drawSelectedButton button c =
       let outerBox = (color red $ drawRectangle [(x-3, y-3), (x2+3, y2+3)])
           [(x, y), (x2, y2)] = buttonShape button
       in Pictures [outerBox, drawButton' button c]
 
     drawButton' :: Button -> Maybe Color -> Picture -- Take 2 corners and return a rectangle shaped button
-    drawButton' button Nothing = color (buttonColor button) $ drawRectangle (buttonShape button) -- Take the default color
-    drawButton' button (Just c) = color c $ drawRectangle (buttonShape button) -- Change the color (e.g. Button is selected)
+    drawButton' button c = Pictures [b (maybe (buttonColor button) id c), s]
+      where
+        [(x, y), (x2, y2)] = buttonShape button
+        s = translate (x + (x2 - x) / 4) (y + 2) $ Scale 0.4 0.4 $ color white $ text (buttonText button)
+        b c = color c $ drawRectangle (buttonShape button)
+
+
+{--
+    drawButton' button Nothing = 
+        let 
+            [(x, y), (x2, y2)] = buttonShape button
+            b = color (buttonColor button) $ drawRectangle (buttonShape button) 
+            s = translate (x + (x2 - x)/4) (y+2) $ Scale 0.4 0.4 $ color white $ text (buttonText button)
+        in Pictures [b, s] -- Take the default color
+    drawButton' button (Just c) = 
+        let 
+            [(x, y), (x2, y2)] = buttonShape button
+            b = color c $ drawRectangle (buttonShape button)
+            s = translate (x + (x2 - x)/4) (y+2) $ Scale 0.4 0.4 $ color white $ text (buttonText button)
+        in Pictures [b, s] -- Change the color (e.g. Button is selected)
+        --}
