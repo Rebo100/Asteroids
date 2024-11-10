ac45aca136618407c6e0c3a688a7e7f59c3d0b53
Rebo100 <78788432+Rebo100@users.noreply.github.com>
2024-11-10 14:55:52 +0100

    nog meer wtf

--- src/Score/Score.hs
+++ src/Score/Score.hs
@@ -1,11 +1,10 @@
 module Score.Score where
 
-import System.IO (appendFile)
+import qualified System.IO.Strict as S (appendFile, readFile, run)
 import System.Directory (createDirectoryIfMissing)
 import Objects.Entities.Entity
 import Model
 import Objects.Entities.Ship
-import System.IO (readFile)
 import System.Directory (doesFileExist)
 import Data.List (sort)
 import Text.Read (readMaybe)
@@ -13,8 +12,8 @@ import Data.Maybe (mapMaybe)
 
 writeScoreToFile :: Int -> IO ()
 writeScoreToFile score = 
-    createDirectoryIfMissing True "Score" >> -- Create directory if it isnt there
-    appendFile "Score/Highscores.txt" (show score ++ "\n") -- Write the new score inside the file
+    createDirectoryIfMissing True "Score" >>
+    S.run (S.appendFile "Score/Highscores.txt" (show score ++ "\n")) -- Write the new score inside the file
 
 getPlayerScore :: GameState -> Int
 getPlayerScore gstate = case findPlayerShipp (entities gstate) of
@@ -22,12 +21,8 @@ getPlayerScore gstate = case findPlayerShipp (entities gstate) of
     _ -> 0  -- Default
 
 readScores :: IO [Int]
-readScores = do
-    exists <- doesFileExist "Score/Highscores.txt" -- Check if file exists
-    if exists then do
-        content <- readFile "Score/Highscores.txt"
+readScores =
+    S.run (S.readFile "Score/Highscores.txt" >>= \content ->
         let scores = mapMaybe readMaybe (lines content) :: [Int]
-            sortedScores = take 3 $ reverse $ sort scores
-        return sortedScores -- Get the top 3 scores
-    else
-        return [] -- return emmpty list
+            sortedScores = take 3 $ reverse $ sort scores 
+        in return sortedScores)
